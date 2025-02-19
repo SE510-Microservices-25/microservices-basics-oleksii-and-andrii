bcc686b7fcb414df5cecbc8ba80e133f21462ed0
Oleksii Stepanyk <o.stepanik06@gmail.com>
2025-02-19 23:25:10 +0200

    compose modify and migration update

--- Vote Service/Program.cs
+++ Vote Service/Program.cs
@@ -12,6 +12,22 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var dbContext = services.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+        Console.WriteLine("Database migrated successfully!");
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
